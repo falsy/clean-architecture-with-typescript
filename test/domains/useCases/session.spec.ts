9e338250eab86d4085a3d3bc5ba1fc7d00d2a2ed
falsy <me@falsy.me>
2020-08-10 00:00:06 +0900

    Update session.spec.ts

--- test/domains/useCases/session.spec.ts
+++ test/domains/useCases/session.spec.ts
@@ -1,5 +1,4 @@
 import { ISessionUseCase } from '@domains/useCases/interfaces/iSession';
-import { ISessionRepository } from '@adapters/repositories/interfaces/iSession';
 import { ISessionVO } from '@domains/vos/interfaces/iSession';
 import { ITokenDTO } from  "@adapters/infrastructures/interfaces/iRemote";
 import SessionUseCase from '@domains/useCases/Session';
@@ -35,29 +34,6 @@ describe('session use case', () => {
     const responseData = await sessionUseCase.login(userData);
 
     expect(responseData).toEqual(mockToken);
-  });
-
-  test('request user token, login() - fail', async () => {
-    const userData: ISessionVO = { id: '', pw: '' };
-    const responseData = await sessionUseCase.login(userData);
-
-    expect(responseData).toEqual('');
-  });
-
-  test('get user token, getToken()', async () => {
-    const userData: ISessionVO = { id: 'id', pw: 'pw' };
-    const responseData = await sessionUseCase.login(userData);
-    const token = sessionUseCase.getToken();
-    
-    expect(token).toEqual(mockToken);
-  });
-
-  test('test for the addToken method', () => {
-    expect(sessionUseCase.addToken).toBeDefined();
-  });
-
-  test('test for the removeToken method', () => {
-    expect(sessionUseCase.removeToken).toBeDefined();
   });
 
 });
