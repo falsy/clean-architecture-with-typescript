6f10e9c21a6d05e3f92f5588872fa9dadc7b4da6
조용석 <roger@antigravity.kr>
2022-11-24 23:13:51 +0900

    style: Board.ts 파일 prettier 적용

--- src/adapters/repositories/Board.ts
+++ src/adapters/repositories/Board.ts
@@ -1,33 +1,30 @@
-import { IBoardRepository } from '@domains/useCases/repository-interfaces/iBoard'
-import BoardDTO, { IBoardDTO, IBoardParams } from "@domains/dto/BoardDTO"
-import CommentDTO, { ICommentDTO, ICommentParams } from '@domains/dto/CommentDTO'
-import { IHttp } from '@adapters/infrastructures/interfaces/iHttp'
+import { IBoardRepository } from '@domains/useCases/repository-interfaces/iBoard';
+import BoardDTO, { IBoardDTO, IBoardParams } from '@domains/dto/BoardDTO';
+import CommentDTO, { ICommentDTO, ICommentParams } from '@domains/dto/CommentDTO';
+import { IHttp } from '@adapters/infrastructures/interfaces/iHttp';
 
 class BoardRepository implements IBoardRepository {
-
-  constructor(
-    readonly http: IHttp
-  ) {}
+  constructor(readonly http: IHttp) {}
 
   async getBoards(): Promise<Array<IBoardDTO>> {
     const response = await this.http.request({
       method: 'GET',
-      url: 'http://localhost:7777/boards'
-    })
+      url: 'http://localhost:7777/boards',
+    });
 
-    if(response?.boards) {
-      return response.boards.map((board: IBoardParams) => new BoardDTO(board))
+    if (response?.boards) {
+      return response.boards.map((board: IBoardParams) => new BoardDTO(board));
     }
   }
 
   async getComments(): Promise<Array<ICommentDTO>> {
     const response = await this.http.request({
       method: 'GET',
-      url: 'http://localhost:7777/comments'
-    })
+      url: 'http://localhost:7777/comments',
+    });
 
-    if(response?.comments) {
-      return response.comments.map((comment: ICommentParams) => new CommentDTO(comment))
+    if (response?.comments) {
+      return response.comments.map((comment: ICommentParams) => new CommentDTO(comment));
     }
   }
 
@@ -36,15 +33,14 @@ class BoardRepository implements IBoardRepository {
       method: 'POST',
       url: 'http://localhost:7777/boards',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: {
-        author, 
-        content
-      }
-    })
+        author,
+        content,
+      },
+    });
   }
-
 }
 
-export default BoardRepository
+export default BoardRepository;
