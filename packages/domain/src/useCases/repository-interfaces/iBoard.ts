6b3573015f7d0535297343299604e8e3f3f843af
CheolJe <me@falsy.me>
2024-06-09 10:58:50 +0900

    feat: clean up repository code before updating version 3.0

--- packages/domain/src/useCases/repository-interfaces/iBoard.ts
+++
@@ -1,8 +0,0 @@
-import { IBoardDTO } from '../../dto/BoardDTO'
-import { ICommentDTO } from '../../dto/CommentDTO'
-
-export interface IBoardRepository {
-  getBoards(): Promise<Array<IBoardDTO>>
-  insertBoard(author: string, content: string): Promise<boolean>
-  getComments(): Promise<Array<ICommentDTO>>
-}
--- packages/domain/src/useCases/repository-interfaces/iSession.ts
+++
@@ -1,8 +0,0 @@
-import { IUserDTO } from '../../dto/UserDTO'
-
-export interface ISessionRepository {
-  login(userDTO: IUserDTO): Promise<string>
-  getToken(): Promise<string>
-  setToken(token: string): void
-  removeToken(): void
-}
